a88ad32adb9ee2c2291935a181856c94ad46814e
Oleg Fedotov <trevor706@gmail.com>
2022-09-03 14:08:52 +0300

    CharacterTAbleViewCell model

---
+++ Breaking Bad Characters/Models/CharacterTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  CharacterTableViewCell.swift
+//  Breaking Bad Characters
+//
+//  Created by Olegio on 03.09.2022.
+//
+
+import UIKit
+
+class CharacterTableViewCell: UITableViewCell {
+
+    @IBOutlet var characterName: UILabel!
+    @IBOutlet var characterNickname: UILabel!
+    @IBOutlet var characterImage: UIImageView!
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
